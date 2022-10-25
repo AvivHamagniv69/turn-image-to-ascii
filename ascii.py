d20e15b597ebbdb0c1f74c4f81aac41d31ce5b9d
AvivHamagniv <51734410+AvivHamagniv69@users.noreply.github.com>
2022-10-25 20:44:51 +0300

    fixed drawing bug
    previously i would increase the contrast number as it was writing to the file, now it does that before so it dosen't have that artifact

--- ascii.py
+++ ascii.py
@@ -9,8 +9,19 @@ width1, height1 = im.size
 ratio = width1/height1
 ratio = round(ratio)
 print(ratio)
-size = int(input("enter the size of image (ratio will be kept)"))
-new_im = im.resize((round(size*4), round(size)))
+size = int(input("enter the size of image (ratio will be kept) "))
+
+num_to_decide_ratio = 4
+
+if ratio/2 > 1:
+    num_to_decide_ratio = num_to_decide_ratio/(ratio/2)
+
+if ratio/2 < 1:
+    num_to_decide_ratio = num_to_decide_ratio/(ratio*2)
+
+print("num to decide ratio = " , num_to_decide_ratio)
+
+new_im = im.resize((round(size*num_to_decide_ratio), round(size)))
 print(new_im.size)
 
 pix = new_im.load()
@@ -21,23 +32,30 @@ ascii_chars = list(ascii_chars)
 len_ascii = len(ascii_chars)
 print(ascii_chars)
 width, height = new_im.size
-arr = []
 num_to_multiply = 1
 
-for h in range(height):
-    try:
-        for row in range(width):
+try:
+    for n in range(height):
+        for i in range(width):
             pixelRGB = new_im.getpixel((row,h))
             r,g,b = pixelRGB
             brightness = (r+g+b)/3 
             ascii_to_write = ascii_chars[floor(brightness/(len_ascii*num_to_multiply))]
 
-            file_of_ascii.write(ascii_to_write)
-        file_of_ascii.write("\n")
+except:
+    num_to_multiply = num_to_multiply+1
+    n = 0
+    i = 0
 
-    except:
-        num_to_multiply = num_to_multiply+1
-        row = row - 1
-        file_of_ascii.write("\n")
+print(num_to_multiply)
+for h in range(height):
+    for row in range(width):
+        pixelRGB = new_im.getpixel((row,h))
+        r,g,b = pixelRGB
+        brightness = (r+g+b)/3 
+        ascii_to_write = ascii_chars[floor(brightness/(len_ascii*num_to_multiply))]
+
+        file_of_ascii.write(ascii_to_write)
+    file_of_ascii.write("\n")
 
 file_of_ascii.close()
